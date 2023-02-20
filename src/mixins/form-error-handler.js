a70faad1413fe833fe921d87318c2fc52a975f59
joaofelipe <joaofelipe1294@gmail.com>
2023-02-20 11:48:46 -0300

    feat: add error handler

--- src/mixins/form-error-handler.js
+++ src/mixins/form-error-handler.js
@@ -1,25 +1,13 @@
 const formErrorHandler = {
   methods: {
-    handleError (field, attributeName) {
-      if (field.$errors.length === 0) { return undefined }
-      const fieldsWithErrors = field.$errors.map(error => error.$property)
-      if (!fieldsWithErrors.includes(attributeName)) { return undefined }
-
-      const message = field.$errors[0].$message
-      if (message === 'Value must be numeric') return 'Valor deve ser numérico'
-      if (message === 'Value must be email') return 'Valor deve ser um email válido'
-      if (message === 'Value is required') return 'Não pode ficar em branco'
-      if (message === 'Date must be valid') return 'Data precisa ser válida'
-      if (field.$errors[0].$validator === 'maxLength') {
-        const maxCharsNumber = field.$errors[0].$params.max
-        return `Número máximo de carcateres: ${maxCharsNumber}`
-      }
-    },
-
     handleResponseError (error) {
       let errorMessage = null
       if (error.response.data.errors) {
-        errorMessage = error.response.data.errors.join(' ')
+        const errors = error.response.data.errors
+        errorMessage = Object.keys(errors).map(key => {
+          return `${key} ${errors[key]}`
+        }).join('\n')
+
       } else {
         errorMessage = error.response.data.message
       }
