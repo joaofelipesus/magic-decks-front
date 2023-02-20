779d1277b5911500d533045a3dc333b1fbc47ada
joaofelipe <joaofelipe1294@gmail.com>
2023-02-19 22:38:04 -0300

    feat: add form components

---
+++ src/components/form/InputField.vue
@@ -0,0 +1,73 @@
+<template>
+  <form-session class="input-field">
+    <label data-test="input-field-label">{{ label }}</label>
+    <input
+      :type="type"
+      class="form-control"
+      @input="(event) => this.$emit('input', event.target.value)"
+      :value="value"
+      :class="hasError(errorMessage)"
+      :disabled="disabled"
+      :data-test="dataTest"
+      :step="step"
+    />
+    <span
+      v-if="errorMessage != undefined"
+      class="text-danger"
+      data-test="input-field-error-message"
+    >
+      {{ errorMessage }}
+    </span>
+  </form-session>
+</template>
+
+<script>
+import FormSession from '@/components/form/FormSession.vue'
+export default {
+  components: { FormSession },
+  props: {
+    label: {
+      required: true,
+      type: String
+    },
+
+    value: {
+      type: [String, Number],
+      default: ''
+    },
+
+    errorMessage: {
+      type: String,
+      default: undefined
+    },
+
+    type: {
+      type: String,
+      default: 'text'
+    },
+
+    step: {
+      type: String,
+      default: undefined
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    dataTest: {
+      type: String,
+      default: 'input-field-input'
+    }
+  },
+
+  methods: {
+    hasError (errorMessage) {
+      if (errorMessage === undefined) return ''
+
+      return 'is-invalid'
+    }
+  }
+}
+</script>
