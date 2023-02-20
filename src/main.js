88af2d121c90aff3785bee4fc9a150db15b5e699
joaofelipe <joaofelipe1294@gmail.com>
2023-02-19 22:21:13 -0300

    chore: config bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './styles/app.scss'
 
 Vue.config.productionTip = false
 
