8fe734d896091d04a90c01dd6470e6f9442734f6
joaofelipe <joaofelipe1294@gmail.com>
2023-02-20 12:13:58 -0300

    feat: add deck list base

--- src/views/deck/DeckList.vue
+++ src/views/deck/DeckList.vue
@@ -2,8 +2,31 @@
   <main>
     <home-navbar />
 
-    <div class="container">
-      AKI!
+    <div class="container pt-5 d-flex">
+      <div
+        v-for="deck in decks"
+        :key="deck.id"
+        class="card col-3 me-2"
+      >
+        <div class="card-body">
+          <h3 class="mb-2 text-center">{{deck.name}}</h3>
+          <div class="mb-3 d-flex justify-content-center">
+            <h6
+              class="badge rounded-pill text-center"
+              :class="formatCollor(deck.format)"
+            >
+              {{deck.format}}
+            </h6>
+          </div>
+
+          <router-link
+            :to="{ name: 'deck-show', params: { id: deck.id } }"
+            class="btn btn-outline-info w-100"
+          >
+            More info
+          </router-link>
+        </div>
+      </div>
     </div>
   </main>
 </template>
@@ -31,13 +54,27 @@ export default {
   mounted () {
     this.listDecks()
       .then(response => {
-        console.log(response)
+        this.decks = response.data.map(deck => {
+          return {
+            id: deck.id,
+            name: deck.name,
+            format: deck.format,
+            description: deck.description
+          }
+        })
       })
       .catch(e => this.handleLoadError(e))
   },
 
   methods: {
-    ...mapActions(['listDecks'])
+    ...mapActions(['listDecks']),
+
+    formatCollor (format) {
+      if (format === 'standard') { return 'bg-primary' }
+      if (format === 'modern') { return 'bg-success' }
+      if (format === 'commander') { return 'bg-secondary' }
+      return ''
+    }
   }
 }
 </script>
