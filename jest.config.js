69a18db0a776a0377d1a7d9ebfc4e6d2002c48aa
joaofelipe <joaofelipe1294@gmail.com>
2023-02-20 12:13:16 -0300

    tests: add actions coverage

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,18 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
-  collectCoverage: true
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js',
+    '!src/lib/api.js',
+    '!src/router/index.js',
+    '!src/App.vue',
+    '!src/store/**/state.js',
+    '!src/store/index.js',
+    '!src/mixins/form-error-handler.js',
+    '!src/mixins/load-error-handler.js',
+    '!src/store/**/index.js',
+    '!src/components/HomeNavbar.vue',
+    '!src/components/form/FormSession.vue'
+  ]
 }
