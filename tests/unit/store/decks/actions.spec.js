b5f4d5def9b9c3fc3b01648695793887fbd074a6
joaofelipe <joaofelipe1294@gmail.com>
2023-02-20 12:13:27 -0300

    tests: add create deck action coverage

--- tests/unit/store/decks/actions.spec.js
+++ tests/unit/store/decks/actions.spec.js
@@ -11,4 +11,22 @@ describe('decksStore', () => {
       expect(api.get).toHaveBeenCalledWith('/decks')
     })
   })
+
+  describe('#createDeck', () => {
+    it('calls the correct URL', async () => {
+      api.post = jest.fn()
+
+      await actions.createDeck(expect.anything(), {
+        name: 'Test Deck',
+        format: 'commander',
+        description: 'Some cool descrition'
+      })
+
+      expect(api.post).toHaveBeenCalledWith('/decks', {
+        name: 'Test Deck',
+        format: 'commander',
+        description: 'Some cool descrition'
+      })
+    })
+  })
 })
