9264a15435cb1770815e15be1c24643236fd2cb0
joaofelipe <joaofelipe1294@gmail.com>
2023-02-19 22:48:00 -0300

    feat: add deck list route.

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import DeckList from '@/views/deck/DeckList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/decks',
+    name: 'deck-list',
+    component: DeckList
+  },
+  {
+    path: '/',
+    redirect: '/decks'
+  }
 ]
 
 const router = new VueRouter({
