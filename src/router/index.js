6508a958222b731b094af237c72024324cd161f1
joaofelipe <joaofelipe1294@gmail.com>
2023-02-26 11:44:44 -0300

    feat: add DeckShow route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import DeckList from '@/views/deck/DeckList.vue'
 import DeckUpsert from '@/views/deck/DeckUpsert.vue'
+import DeckShow from '@/views/deck/DeckShow.vue'
 
 Vue.use(VueRouter)
 
@@ -14,6 +15,11 @@ const routes = [
     path: '/decks',
     name: 'deck-list',
     component: DeckList
+  },
+  {
+    path: '/decks/:id',
+    name: 'deck-show',
+    component: DeckShow
   },
   {
     path: '/decks/new',
