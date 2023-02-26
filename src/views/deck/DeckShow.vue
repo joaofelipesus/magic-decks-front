e14618cff4e7ffc6385e53170983046e75f448c0
joaofelipe <joaofelipe1294@gmail.com>
2023-02-26 11:31:13 -0300

    feat: add deck show view

---
+++ src/views/deck/DeckShow.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <home-navbar class="mb-4"/>
+
+    <div class="container">
+      <card-search />
+    </div>
+  </main>
+</template>
+
+<script>
+import HomeNavbar from '@/components/HomeNavbar.vue'
+import CardSearch from '@/components/deck/CardSearch.vue'
+
+export default {
+  name: 'DeckShow',
+
+  components: {
+    CardSearch,
+    HomeNavbar
+  },
+}
+</script>
