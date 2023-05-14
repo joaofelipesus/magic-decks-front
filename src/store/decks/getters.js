bef30d861bfc7a7b40e5291b129f46dbc7086602
joaofelipe <joaofelipe1294@gmail.com>
2023-05-14 14:53:02 -0300

    feat: add sorted name

--- src/store/decks/getters.js
+++ src/store/decks/getters.js
@@ -8,7 +8,12 @@ export default {
   },
 
   getDeckCards (state) {
-    return state.deck?.deck_cards
+    return state.deck?.deck_cards?.sort((a, b) =>  {
+      const firstCardName = a.card.name_en || a.card.name_pt
+      const secondCardName = b.card.name_en || b.card.name_pt
+
+      return (firstCardName > secondCardName) ? 1 : -1
+    })
   },
 
   getDeckFormat(state) {
--- tests/unit/store/decks/getters.spec.js
+++ tests/unit/store/decks/getters.spec.js
@@ -4,7 +4,10 @@ describe('decksStore.getters', () => {
   const state = {
     deck: {
       name: 'Test Deck',
-      deck_cards: [ {id: 1, name: 'Test Card 1'}, {id: 2, name: 'Test Card 2'}],
+      deck_cards: [
+        { id: 1, card: { name_en: 'Test Card A'} },
+        { id: 2, card: { name_en: 'Test Card B' } }
+      ],
       format: 'commander'
     }
   }
@@ -24,8 +27,8 @@ describe('decksStore.getters', () => {
   describe('getDeckCards', () => {
     it('returns deck cards from state', () => {
       expect(getters.getDeckCards(state)).toEqual([
-        {id: 1, name: 'Test Card 1'},
-        {id: 2, name: 'Test Card 2'}
+        {id: 1, card: { name_en: 'Test Card A' } },
+        {id: 2, card: { name_en: 'Test Card B'} }
       ])
     })
   })
