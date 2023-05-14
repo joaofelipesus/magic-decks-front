259dcb779830d6cd2d200192170bb4f2a90ce166
joaofelipe <joaofelipe1294@gmail.com>
2023-05-14 11:50:12 -0300

    feat: add mutation that add new card to deck

--- src/store/decks/mutations.js
+++ src/store/decks/mutations.js
@@ -1,5 +1,11 @@
 export default {
   setDeck (state, deck) {
     state.deck = deck
+  },
+
+  addDeckCard(state, card) {
+    const cardsList = state.deck.deck_cards
+    cardsList.push(card)
+    state.deck.deck_cards = cardsList
   }
 }
--- tests/unit/store/decks/mutations.spec.js
+++ tests/unit/store/decks/mutations.spec.js
@@ -12,4 +12,18 @@ describe('decksStore.mutations', () => {
       expect(state.deck).toEqual({name: 'Test Deck'})
     })
   })
+
+  describe('addDeckCard', () => {
+    it('adds a card to the deck', () => {
+      const state = {
+        deck: {
+          deck_cards: []
+        }
+      }
+
+      mutations.addDeckCard(state, {name: 'Test Card'})
+
+      expect(state.deck.deck_cards).toEqual([{name: 'Test Card'}])
+    })
+  })
 })
